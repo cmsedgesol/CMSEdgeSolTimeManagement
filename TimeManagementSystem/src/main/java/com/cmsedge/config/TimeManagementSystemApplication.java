361c125d234857a7df0be0c778470858a30c0c8b
rohith <rohithdurgum@gmail.com>
2018-03-23 14:53:48 +0530

    commiting changes

--- TimeManagementSystem/src/main/java/com/cmsedge/config/TimeManagementSystemApplication.java
+++ TimeManagementSystem/src/main/java/com/cmsedge/config/TimeManagementSystemApplication.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
-import org.slf4j.Logger;
+import org.slf4j.Logger; 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
