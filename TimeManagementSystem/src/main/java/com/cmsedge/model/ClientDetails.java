0af9d2a3427c19e4a4fb3179a35842ba85f10fd3
seshujava72 <seshujava72@gmail.com>
2018-03-28 11:15:02 +0530

    adding pojo classes

--- TimeManagementSystem/src/main/java/com/cmsedge/model/ClientDetails.java
+++ TimeManagementSystem/src/main/java/com/cmsedge/model/ClientDetails.java
@@ -6,11 +6,11 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Client_Details")
+@Table(name = "Client_Details")
 public class ClientDetails {
 
 	@Id
-	@Column(name = "Client_ID", nullable = false)	
+	@Column(name = "Client_ID", nullable = false)
 	private String clientID;
 	@Id
 	@Column(name = "Client_Name", nullable = false)
@@ -18,4 +18,29 @@ public class ClientDetails {
 	@Id
 	@Column(name = "Client_Description", nullable = true)
 	private String clientDescription;
+
+	public String getClientID() {
+		return clientID;
+	}
+
+	public void setClientID(String clientID) {
+		this.clientID = clientID;
+	}
+
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+
+	public String getClientDescription() {
+		return clientDescription;
+	}
+
+	public void setClientDescription(String clientDescription) {
+		this.clientDescription = clientDescription;
+	}
+
 }
